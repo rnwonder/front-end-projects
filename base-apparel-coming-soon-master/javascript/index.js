896847f80f934a92102d4277e3d804ed68a34cb8
rnwonder <rnwonder@gmail.com>
2021-01-21 04:49:26 +0100

    Base Apparel

---
+++ base-apparel-coming-soon-master/javascript/index.js
@@ -0,0 +1,41 @@
+// ACCESS
+
+const formInput = document.getElementById('email');
+const button = document.getElementById('submit-btn')
+const form = document.getElementById('form')
+const small = form.querySelector('small')
+let valid = false
+
+
+form.addEventListener('submit', (e)=>{
+  e.preventDefault()
+
+  checkEmail(email.value)
+
+  if (valid) {
+    form.submit()
+  }
+  
+})
+
+
+const checkEmail = (input) => {
+  if(!validate(input)) {
+    small.innerText = 'Please provide a valid email !'
+
+    formInput.classList.add('error')
+    form.classList.add('normal')
+  } else {
+    formInput.classList.remove('error')
+    form.classList.remove('normal')
+    valid = true
+  }
+
+}
+
+
+const validate = (email) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(email.toLowerCase())
+}
