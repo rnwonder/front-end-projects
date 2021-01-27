46bd42f8a575f68dfc650a2a30a17af67843506f
rnwonder <rnwonder@gmail.com>
2021-01-27 04:48:38 +0100

    manage landing page

---
+++ manage-landing-page-master/javascript/index.js
@@ -0,0 +1,122 @@
+// ACCESS NAV 
+const menu = document.getElementById('menu')
+const close = document.getElementById('close')
+const navDropDown = document.getElementById('nav-bar')
+const background = document.getElementById('bg-color')
+const menuParent = menu.parentElement
+const closeParent = close.parentElement
+
+let cl = [close, background]
+
+
+// ACCESS SCROLL
+const scroll = document.querySelector('.scroll')
+const scrollWidth = scroll.offsetWidth
+const position1 = document.getElementById('one')
+const position2 = document.getElementById('two')
+const position3 = document.getElementById('three')
+const position4 = document.getElementById('four')
+
+
+// ACCESS FORM
+const form = document.getElementById('form')
+const small = form.querySelector('small')
+const email = document.getElementById('update')
+let valid = false
+
+
+
+
+
+// NAV LOGIC
+menu.addEventListener('click', ()=>{
+  menuParent.classList.add('desk')
+  background.classList.add('active')
+  closeParent.classList.remove('desk')
+  navDropDown.classList.remove('desk')
+  
+})
+
+for(let i = 0; i<cl.length; i++) {
+  cl[i].addEventListener('click', ()=>{
+    menuParent.classList.remove('desk')
+    background.classList.remove('active')
+    closeParent.classList.add('desk')
+    navDropDown.classList.add('desk')
+  })
+}
+
+
+
+
+
+// SCROLL LOGIC
+scroll.addEventListener('scroll', ()=>{
+
+    if (scroll.scrollLeft < scrollWidth) {
+      position1.classList.add('fill') 
+      position2.classList.remove('fill') 
+      position3.classList.remove('fill') 
+      position4.classList.remove('fill')    
+    } 
+
+    if (scroll.scrollLeft >= scrollWidth - 30 && scroll.scrollLeft < (scrollWidth - 30) * 2) {
+      position1.classList.remove('fill') 
+      position2.classList.add('fill') 
+      position3.classList.remove('fill') 
+      position4.classList.remove('fill') 
+    }
+
+    if (scroll.scrollLeft >= (scrollWidth - 30) * 2 && scroll.scrollLeft < (scrollWidth - 30) * 3) {
+      position1.classList.remove('fill') 
+      position2.classList.remove('fill') 
+      position3.classList.add('fill') 
+      position4.classList.remove('fill') 
+    }
+
+    if (scroll.scrollLeft >= (scrollWidth - 30) * 3) {
+      position1.classList.remove('fill') 
+      position2.classList.remove('fill') 
+      position3.classList.remove('fill') 
+      position4.classList.add('fill') 
+    }
+
+})
+
+
+if (scrollWidth < 800) {
+  scroll.scrollLeft = (scrollWidth + 28) * 1
+} else {
+  scroll.scrollLeft = 0.1 * scrollWidth
+}
+
+
+
+
+
+// FORM LOGIC
+form.addEventListener('submit' , (e)=>{
+  e.preventDefault()
+
+  checkInput(email.value)
+
+  if(valid) {
+    form.submit()
+  }
+})
+
+const checkInput = (input) => {
+  if(!validateEmail(input)) {
+    form.classList.add('error')
+    small.innerText = 'Please enter a valid email'
+  } else {
+    form.classList.remove('error')
+    valid = true
+  }
+}
+
+
+const validateEmail = (input) => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(input.toLowerCase())
+}
